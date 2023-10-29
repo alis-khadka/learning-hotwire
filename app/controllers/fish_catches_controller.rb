11eb481c57dd2089f65cc2f109c15a38d5d98f60
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-10-29 22:04:59 +0545

    feat(): handle turbo_stream and html request accordingly while deleting fish_catch

--- app/controllers/fish_catches_controller.rb
+++ app/controllers/fish_catches_controller.rb
@@ -44,8 +44,12 @@ class FishCatchesController < ApplicationController
   def destroy
     @fish_catch.destroy
 
-    @fish_catches = fish_catches_for_bait(@fish_catch.bait)
-    # redirect_to tackle_box_item_for_catch(@fish_catch)
+    respond_to do |format|
+      format.html { redirect_to tackle_box_item_for_catch(@fish_catch) }
+      format.turbo_stream do
+        @fish_catches = fish_catches_for_bait(@fish_catch.bait)
+      end
+    end
   end
 
 private
