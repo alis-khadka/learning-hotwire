5ce33ced827808f86bfe1a3a8a82757111e2640d
Prashant Khadka <25191509+alis-khadka@users.noreply.github.com>
2023-10-31 16:19:08 +0545

    chore(): observe other turbo events

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,11 @@ import "./controllers"
 
 // Turbo.session.drive = false
 // Turbo.setProgressBarDelay(0.5)
+
+window.addEventListener("turbo:frame-render", (e) => {
+    console.log("turbo:frame-render", e)
+})
+
+window.addEventListener("turbo:frame-load", (e) => {
+    console.log("turbo:frame-load", e)
+})
